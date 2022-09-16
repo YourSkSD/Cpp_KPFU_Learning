c0a3b8b253cbf388be7fcde9d5bb6460bd507a19
Artur Davydov <zillo236@gmil.com>
2022-09-16 21:50:32 +0300

    Second completed

--- Practice_second/iter.hpp
+++ Practice_second/iter.hpp
@@ -1,6 +1,6 @@
-template<class T1, class T2> // Шаблон, тип данный определяется при подстановке переменных во время вызова функции
-void iter(T1 *array, int size, T2 (*foo)(T1 array)) {
-	for (int i = 0; i < size; i++) {
-		
+template<class T1> // Шаблон, тип данный определяется при подстановке переменных во время вызова функции
+void iter(T1 *array, int size, T1 (*foo)(T1)) {
+	for (int i = 0; i < size; ++i) {
+		array[i] = foo(array[i]);
 	}
 }
